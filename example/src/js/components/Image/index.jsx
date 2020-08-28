c7fc82665b583178fc8817d14e930d4245658c06
Rafael <rafael.chaves98@hotmail.com>
2020-08-28 14:36:02 -0300

    Small fixes

--- example/src/js/components/Image/index.jsx
+++ example/src/js/components/Image/index.jsx
@@ -1,15 +1,39 @@
 // Packages
 import React from 'react';
 
-export default function NotFound () {
+// Img
+import fallbackImg from '../../../img/default.svg';
+
+export default function Image ({context = "", src, ...props}) {
+
+	//-------------------------------------------------
+	// Properties
+	//-------------------------------------------------
+
+	// states
+	const [image, setimage] = React.useState();
+
+	//-------------------------------------------------
+	// Effects
+	//-------------------------------------------------
+
+	React.useEffect(() => {
+		try {
+			require(context + '/' + src)
+			.then(img => {
+				setimage(img);
+			});
+		}
+		catch (e) {
+			console.log(e);
+		}
+	}, [src, context]);
 
 	//-------------------------------------------------
 	// Render
 	//-------------------------------------------------
 
 	return (
-		<div>
-			hi
-		</div>
+		<img {...props} src={image || fallbackImg} />
 	);
 }
